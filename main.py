a159561d45ab2578f5c7ea08f27eb8cf6151266e
Alitpl41 <alitpl41@gmail.com>
2023-12-17 23:50:39 +0300

    d

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import streamlit as st
 import plotly.graph_objs as go 
-from plotly.offline import init_notebook_mode,iplot
+from plotly.offline import init_notebook_mode,iplot,plot
 import pandas as pd
 
 # init_notebook_mode(connected=True) 
 
-
+st.title('2014 World Power Consumption')
 df = pd.read_csv('2014_World_Power_Consumption')
 # df.head()
 
@@ -21,4 +22,6 @@ layout = dict(title='Power Consumption', geo=dict(showframe=False,
         projection = {'type':'bottomley'}))
 
 choromap = go.Figure(data = [data],layout = layout)
-iplot(choromap,validate=False)
+st.write(iplot(choromap,validate=False))
+
+# st.plotly_chart(iplot(choromap,validate=False))
